c661952775180c2505609134287801bb318be129
David Marten <dvmtn@pm.me>
2024-09-26 14:33:29 -0500

    Fancified logo

--- src/lib/icons/CanaryLayered.svelte
+++ src/lib/icons/CanaryLayered.svelte
@@ -6,22 +6,22 @@
 <svg xmlns="http://www.w3.org/2000/svg" {width} {height} fill-rule="evenodd" viewBox="0 0 282 235">
 	<path
 		class="m"
-		fill="#f0f"
+		fill="var(--color-accent-secondary)"
 		d="M205 194.4c-23 23.1-53 39.8-88 39.8a117.1 117.1 0 1 1 116.8-125.9c.2 2.4 37.9 5 48 64 .3 1.9-44.5-10.5-76.9 22.1ZM128.3 40.3a63.1 63.1 0 1 1 0 126.2 63.1 63.1 0 0 1 0-126.2Z"
 	/>
 	<path
 		class="y"
-		fill="#ff0"
+		fill="var(--color-accent-tertiary)"
 		d="M205 194.4c-23 23.1-53 39.8-88 39.8a117.1 117.1 0 1 1 116.8-125.9c.2 2.4 37.9 5 48 64 .3 1.9-44.5-10.5-76.9 22.1ZM128.3 40.3a63.1 63.1 0 1 1 0 126.2 63.1 63.1 0 0 1 0-126.2Z"
 	/>
 	<path
 		class="c"
-		fill="#0ff"
+		fill="var(--color-accent-primary)"
 		d="M205 194.4c-23 23.1-53 39.8-88 39.8a117.1 117.1 0 1 1 116.8-125.9c.2 2.4 37.9 5 48 64 .3 1.9-44.5-10.5-76.9 22.1ZM128.3 40.3a63.1 63.1 0 1 1 0 126.2 63.1 63.1 0 0 1 0-126.2Z"
 	/>
 	<path
-		class="w"
-		fill="#fff"
+		class="k"
+		fill="var(--color-bg)"
 		d="M205 194.4c-23 23.1-53 39.8-88 39.8a117.1 117.1 0 1 1 116.8-125.9c.2 2.4 37.9 5 48 64 .3 1.9-44.5-10.5-76.9 22.1ZM128.3 40.3a63.1 63.1 0 1 1 0 126.2 63.1 63.1 0 0 1 0-126.2Z"
 	/>
 </svg>
@@ -29,10 +29,13 @@
 <style>
 	svg {
 		overflow: visible;
-		font-size: 1.75rem;
+		font-size: 2rem;
 
 		& path {
-			transition: all 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+			transition:
+				fill 1s cubic-bezier(0.34, 1.56, 0.64, 1),
+				transform 1s cubic-bezier(0.34, 1.56, 0.64, 1),
+				opacity 0.2s ease-out;
 		}
 		& > .c {
 			transform: translate(0.4375em, 0.375em);
@@ -43,12 +46,24 @@
 		& > .y {
 			transform: translate(-0.375em, -0.3125em);
 		}
-		& > .w {
+		& > .k {
 			filter: drop-shadow(0.125em 0.125em 0.125em #00000055);
 		}
 		&:hover {
 			& path {
 				transform: none;
+				transition:
+					fill 1s cubic-bezier(0.34, 1.56, 0.64, 1),
+					transform 1s cubic-bezier(0.34, 1.56, 0.64, 1),
+					opacity 0.5s ease-out 0.5s;
+
+				&:not(.k) {
+					opacity: 0;
+				}
+
+				&.k {
+					fill: var(--color-fg);
+				}
 			}
 		}
 	}
